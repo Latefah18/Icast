0420c80dc5b55a9df9318f62e28fbc0f2aa8772a
NorahMansour <norahalsheaifi@gmail.com>
2022-08-07 11:02:39 +0300

    final-new

--- src/components/Login.tsx
+++ src/components/Login.tsx
@@ -17,30 +17,40 @@ interface UserProps {
         const [password,setPassword]=useState<string>("");
         const [error,setError]=useState<string>("");
 
-         const navigate = useNavigate();
-         //const dispatch = useAppDispatch();
-
-         useEffect(() => {
-            if (user) {
-              navigate("/");
-            }
-          }, [navigate, user]);
-          const handleLogin = (e: { preventDefault: () => void }) => {
-            if (email.includes("@")) {
-              if (email.split("@")[1] !== "gmail.com") {
-                e.preventDefault();
-                setError("Gmail account only");
-              } else {
-                const newUser = {
-                  email: email,
-                };
-                localStorage.setItem("user", JSON.stringify(newUser));
-                setUser(newUser);
-              }
-            } else {
-              setError("Need Email");
-            }
+        const handleemailChange =(e:any) => {
+            setEmail( e.target.value);
+           
           };
+
+          let handleChange = (e :any)=> {
+            console.log("handle change is triggering", e.target.value);
+            setPassword(e.target.value);
+          };
+
+        //  const navigate = useNavigate();
+        //  //const dispatch = useAppDispatch();
+
+        //  useEffect(() => {
+        //     if (user) {
+        //       navigate("/");
+        //     }
+        //   }, [navigate, user]);
+        //   const handleLogin = (e: { preventDefault: () => void }) => {
+        //     if (email.includes("@")) {
+        //       if (email.split("@")[1] !== "gmail.com") {
+        //         e.preventDefault();
+        //         setError("Gmail account only");
+        //       } else {
+        //         const newUser = {
+        //           email: email,
+        //         };
+        //         localStorage.setItem("user", JSON.stringify(newUser));
+        //         setUser(newUser);
+        //       }
+        //     } else {
+        //       setError("Need Email");
+        //     }
+        //   };
         
 
 
@@ -63,6 +73,14 @@ interface UserProps {
 
 
 
+        function SubmitButton(e:any){
+            if ( email &&password.length>10){
+              
+              return <button type="button" className="log-btn">الدخول</button>
+            } else {
+              return <button type="button" disabled className="log-btn">الدخول</button>
+            };
+          };
          
 
         return (
@@ -84,20 +102,28 @@ interface UserProps {
            
             </div>
             <br/>
-            <form onSubmit={handleLogin}>
+            <form >
             <div className="l-part" >
-                
-                
-                <input type="text" placeholder=" اسم المستخدم" className="input-1"  required
+          <input className="input-1" type="email" name="Email"placeholder='البريد الإلكتروني'   onChange={handleemailChange}/>
+          {email.includes('@') && email.slice(-4).includes('.com')
+    ? <p>  valid </p>
+    : <p>*should contain (@,.com) </p>}
+          
+      
+        
+                {/* <input type="text" placeholder=" اسم المستخدم" className="input-1"  required
               value={email}
-              onChange={(e) => setEmail(e.target.value)}   />
+              onChange={(e) => setEmail(e.target.value)}   /> */}
 
                 {/* onChange={(e) => setEmail(e.target.value)} */}
 
                 <div className="overlap-text">
-                    <input type="password" placeholder="كلمة المرور" className="input-2" required
+                <input className="input-2" type="password" name="pass"placeholder='كلمة المرور' required onChange={handleChange} />
+        {password.length >10 ? <p> valid </p>: <p> * password shold be more than 10</p>}
+       
+                    {/* <input type="password" placeholder="كلمة المرور" className="input-2" required
               value={password}
-              onChange={(e) => setPassword(e.target.value)} />
+              onChange={(e) => setPassword(e.target.value)} /> */}
                     {/* onChange={(event) => setPassword(event.target.value)} */}
                 </div>
               
@@ -106,8 +132,11 @@ interface UserProps {
                <input type="checkbox" name="checkbox" id="checkbox" />
                <label  className="check-h1" htmlFor="checkbox">تذكرني</label>
            </div>
-               
-           <Link to={"/prodcast"}> <input type="button" value="الدخول" className="log-btn" /></Link>
+           
+           
+
+           
+           <Link to={"/prodcast"}><SubmitButton/> </Link>
                 {/* {error && <span className="login__err">Wrong emai or password !</span>} */}
                 
                 
